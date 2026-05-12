a513e239f77f7341ab052e4969430bfd0b644bc7
Vadim <_s_e_t__@ukr.net>
2026-05-12 13:11:07 +0300

    add some code

--- src/components/MovieGrid/MovieGrid.tsx
+++ src/components/MovieGrid/MovieGrid.tsx
@@ -1,7 +1,22 @@
 import styles from './MovieGrid.module.css'
 
 const MovieGrid = () => {
-    return <>Movie Grid</>
+    return (
+      <ul className={styles.grid}>
+        {/* Набір елементів списку з фільмами */}
+        <li>
+          <div className={styles.card}>
+            <img
+              className={styles.image}
+              src="https://image.tmdb.org/t/p/w500/poster-path"
+              alt="movie title"
+              loading="lazy"
+            />
+            <h2 className={styles.title}>Movie title</h2>
+          </div>
+        </li>
+      </ul>
+    );
 }
 
 export default MovieGrid;
--- src/components/MovieModal/MovieModal.tsx
+++ src/components/MovieModal/MovieModal.tsx
@@ -1,7 +1,30 @@
 import styles from './MovieModal.module.css'
 
 const MovieModal = () => {
-    return <>Movie Modal</>
+    return (
+      <div className={styles.backdrop} role="dialog" aria-modal="true">
+        <div className={styles.modal}>
+          <button className={styles.closeButton} aria-label="Close modal">
+            &times;
+          </button>
+          <img
+            src="https://image.tmdb.org/t/p/original/backdrop_path"
+            alt="movie_title"
+            className={styles.image}
+          />
+          <div className={styles.content}>
+            <h2>movie_title</h2>
+            <p>movie_overview</p>
+            <p>
+              <strong>Release Date:</strong> movie_release_date
+            </p>
+            <p>
+              <strong>Rating:</strong> movie_vote_average/10
+            </p>
+          </div>
+        </div>
+      </div>
+    );
 }
 
 export default MovieModal
